59e2cb9f4cbfb964f490ae5c5caf4a5309d929aa
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2021-11-21 19:43:58 +0530

    Doing Auth for All

--- src/Public/Components/Login/index.js
+++ src/Public/Components/Login/index.js
@@ -6,8 +6,10 @@ function Login() {
         
         <div className="bg-NavbarBg h-screen flex md:flex-row flex-col font-reemKufi">
            <div className="m-auto">
-           <form className="mt-8 space-y-6" action="#" method="POST">
-           <LoginForm/>
+           <form className="mt-8 space-y-6" onSubmit={e=>{
+               console.log(e.target[2].value)
+           }}>
+           <LoginForm />
            </form>
             </div>
       
